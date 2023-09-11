807987f5e34d3a3c9b5c073d8b3751e8c2a6639e
aleroms <santiagomorales333@yahoo.com>
2023-09-11 12:27:25 -0600

    initialized annie's web app

---
+++ README.md
@@ -0,0 +1,10 @@
+Vue web app for Annie's Wood Therapy servicing Southern California
+
+# Annie's Wood Therapy
+
+started on 09/11/2023.
+
+## installation instructions
+sass preprocessor `npm install -D sass`
+
+## useful links
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<template>
+  <header id="header">header</header>
+  <main id="main">
+    <RouterView />
+  </main>
+  <footer id="footer">footer</footer>
+</template>
+
+<style lang="scss"></style>
